0839ca645505a09b968c80df8366c169949c0a4e
Dmitrii Murzin <dmitrii.murzin@ibwave.com>
2017-05-19 09:36:50 -0400

    some updates 2

--- src/hello.erl
+++ src/hello.erl
@@ -8,4 +8,4 @@
 start() ->
     X = io:get_line("What is your name? "),
     Y = string:strip(X, right, $\n),
-    io:fwrite("Hello, what's up, ~s?\n", [Y]).
+    io:fwrite("Hello, what's up Doc, ~s?\n", [Y]).
